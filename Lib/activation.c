8a1f62810f6fcb7ed5acfd5030f71861c9ae67b4
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2023-09-22 01:13:11 +0300

    mul2_1_896_14_14()

--- Lib/activation.c
+++ Lib/activation.c
@@ -39,6 +39,24 @@ mul_1_896_14_14 (const float A[1][896][14][14],
 }
 
 LIB_HIDDEN void
+mul2_1_896_14_14 (const float A[1][896][14][14],
+                  const float B[1][896][14][14],
+                  float       C[1][896][14][14])
+{
+	/* Mul
+	   shift_dir: NOT_GIVEN
+	   fmod: 0
+	 */
+	for (unsigned i1 = 0; i1 < 896; i1++) {
+		for (unsigned i2 = 0; i2 < 14; i2++) {
+			for (unsigned i3 = 0; i3 < 14; i3++) {
+				C[0][i1][i2][i3] = A[0][i1][i2][i3] * B[0][i1][i2][i3];
+			}
+		}
+	}
+}
+
+LIB_HIDDEN void
 sigmoid_1_896_14_14 (const float X[1][896][14][14],
                      float       Y[1][896][14][14])
 {
--- Lib/activation.h
+++ Lib/activation.h
@@ -14,6 +14,11 @@ mul_1_896_14_14 (const float A[1][896][14][14],
                  float       C[1][896][14][14]);
 
 LIB_HIDDEN extern void
+mul2_1_896_14_14 (const float A[1][896][14][14],
+                  const float B[1][896][14][14],
+                  float       C[1][896][14][14]);
+
+LIB_HIDDEN extern void
 sigmoid_1_896_14_14 (const float X[1][896][14][14],
                      float       Y[1][896][14][14]);
 
