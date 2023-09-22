e9878fe03b08f053a37c02548d8a171d6b69b893
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2023-09-22 23:43:03 +0300

    perhaps

--- Lib/dedup.sh
+++ Lib/dedup.sh
@@ -4,6 +4,7 @@ rm -f tmp
 
 h=$(grep -Eo '[[:xdigit:]]{40} +-$' \
          uniq_fn.txt) && h=${h::40} || exit 1
+func="func_${h::11}"
 
 f=($(grep -h "^$h" fn_norm.txt \
      | cut -c49- | cut -d. -f1))
@@ -13,16 +14,17 @@ f=($(grep -h "^$h" fn_norm.txt \
 printf -v rgx -- '|%s' "${f[@]}"
 rgx="layer_moreNet_(${rgx:1})"
 
-sed -Ei "s/$rgx\(/layer_moreNet_${f[0]}(/" model.c
+sed -Ei "s/$rgx\(/$func(/" model.c
 
 { echo;
-  sed 's/^static inline/LIB_HIDDEN/' \
+  sed -E -e 's/^static inline/LIB_HIDDEN/' \
+      -e "s/^layer_moreNet.+ *\(/$func (/" \
       "layer/${f[0]}.h"; } >> activation.c
 
-{ grep -B2 '^layer_moreNet' "layer/${f[0]}.h" \
-  | sed 's/^static inline/LIB_HIDDEN extern/' \
-  | head -c-1; echo ';'; } >> activation.h    &&
-sed -i '/#endif.*/d' activation.h             &&
+{ grep -B1 "^$func" activation.c | head -2 \
+  | sed -E 's/^(LIB_HIDDEN)/\1 extern/'    \
+  | head -c-1; echo ';'; } >> activation.h &&
+sed -i '/#endif.*/d' activation.h          &&
 echo '#endif // ACTIVATION_H_' >> activation.h
 
 printf -v hdr -- 'layer/%s.h\n' "${f[@]}" &&
@@ -32,5 +34,3 @@ echo -n "$hdr" | grep -Fvf- layer.h > tmp &&
 mv tmp layer.h || exit 1
 
 echo -n "$hdr" | xargs git rm
-
-#echo "$rgx"
